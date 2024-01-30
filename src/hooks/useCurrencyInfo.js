7c2402aadc9d5430028629fa92d27ca9e63e92bf
Chanchal010 <Chanchalbag112@gmail.com>
2024-01-30 15:05:10 +0000

    max complete

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,15 @@
+import {useState,useEffect} from 'react'
+
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({})
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
+        console.log(data);
+    },[currency])
+
+  return data
+}
+
+export default useCurrencyInfo;
